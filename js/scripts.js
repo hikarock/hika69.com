7bc965971c705848ebabbd15ba21f9ae5b7fedf1
hikarock <i@hika69.com>
2015-12-20 01:48:32 +0900

    hover をやめる

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
 $(function() {
   FastClick.attach(document.body);
-  $('.hover').on('touchstart touchend', function(evt) {
-    evt.preventDefault();
-    $(this).toggleClass('hover-effect');
-  });
 });
